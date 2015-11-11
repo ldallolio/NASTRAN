2f4dc865037c2d78059afbd5a783074f074fe07f
Luca Dall'Olio <luca.dallolio@gmail.com>
2015-11-11 17:24:35 +0100

    correctly configured XFAIL tests

--- nid/testrunner.sh
+++ nid/testrunner.sh
@@ -7,5 +7,25 @@ export IN12=$1.in12
 export LOGNM=$1.log
 export FT05=$1
 export RFDIR=../rf
+export SOF1="none"
+export SOF2="none"
+export DICTNM="none"
+export FTN11="none"
+export FTN12="none"
+export FTN13="none"
+export FTN14="none"
+export FTN15="none"
+export FTN16="none"
+export FTN17="none"
+export FTN18="none"
+export FTN19="none"
+export FTN20="none"
+export FTN21="none"
+export FTN22="none"
+export FTN23="none"
+export DBMEM="14000000"
+export OCMEM="14000000"
+
+
 export DIRCTY="${TMPDIR:-/tmp}"
 ../mds/nastran
